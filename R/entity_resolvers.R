51697576488e89118d8a05ed697fb82d0e39dcbc
Alex Bresler <abresler@asbcllc.com>
2019-09-15 14:02:06 -0400

    a

--- R/entity_resolvers.R
+++ R/entity_resolvers.R
@@ -32,18 +32,53 @@
 dictionary_entity_slugs <-
   function() {
     words <-
-      c(" & ", " ag ", " and", " as ", " at ", " authority ", " bank ",
+      c("investors$",
+        "INC.ETC$",
+        "ES/OF$",
+        ",INC$",
+        " SAVS ",
+        " FA$",
+        "FEDERATION",
+        " FEDERAL ",
+        "FEDERATION",
+        "SUPERMARKET",
+        "INVESTORS$",
+        " ASSN ",
+        "S&L",
+        "FIRST ",
+        "PRUDENTIAL",
+        "MORTGAGE",
+        "MTGECP",
+        "CREDIT$",
+        " LIBERTY ",
+        " FEDERAL ",
+        "EPISCOPAL",
+        "HSG$",
+        " CP$",
+        " AVE ",
+        " REAL EST ",
+
+        "UNITED NATIONS",
+        "ORGANIZATNINC",
+        "LNASC$",
+        "ENTERPR.,INC$",
+        " CP$",
+        "TRCO.",
+        "H.U.D.$",
+        "PAINEETC$",
+        " (NY) ",
+        " & ", " ag$", " and ", " as ", " at ", " authority ", " bank ",
         " benefit ", " bk", " building ", " ca ", " capital", " center",
-        " city ", " co", " co ", " co.", " corp", " dated ", " east ",
+        " city ", " co", " co ", " co$", " corp", " dated ", " east ",
         " equities", " fee ", " for ", " fund ", " global ", " gp", " gp ",
         " group", " group ", " guardian ", " health ", " holdings", " in ",
         " inc", " inc ", " land ", " living ", " llc", " llc ", " llp ",
         " ltd", " majesty ", " pc", " revocable trust", " services",
         " street ", " street ", " surviving ", " the ", " to ", " trust ",
         " west ", "(estate)", "\\, ab ", "^[0-9]", "$[0-9]", "academy",
-        "acquisition", "admin of est", "administrator", "ag", "amended",
+        "acquisition", "admin of est", "administrator", "$[ag]", "amended",
         "america", "american", "ancillary", "and&prptsinc", "apartment",
-        "apartments", "apartments", "architect", "architects", "as ",
+        "apartments", "apartments", "architect", "architects", " as ",
         "as administrator", "as receiver", "as trustee ", "assembly",
         "asset", "assoc", "associated", "associates", "assocs", "banc",
         "bancorp", "bank", "banorp", "benefit", "building", "capital",
@@ -51,18 +86,18 @@ dictionary_entity_slugs <-
         "church", "college", "community", "company", "congregation",
         "consulate", "corp", "corporation", "corporation", "crp", "cultural",
         "custodian", "dated", "dds", "deacons", "debtor", "defined benefit plan",
-        "department", "dept", "development", "devisee", "devisee", "dpt",
+        "department", "dept ", "development", "devisee", "devisee", "dpt",
         "e/o", "enterprise", "equities", "equity", "est of ", "estate of",
         "exc", "exec", "executrix", "finance", "financial", "financing",
         "foundation", "fsb", "fund", "global", "government", "govt",
         "grammar", "group", "hadassah", "hdfc", "heir at law", "heir-at-law",
         "homes", "hospital", " house ", "housing", "housing development",
-        "immobiliare", "inc", "incorp", "incorporated", "individually",
+        "immobiliare", " inc "," inc$", "incorp", "incorporated", "individually",
         "institute", "insurance", "interest", "international", "inversiones",
         "investment", " jew  ", "jewish", "jv", "land title insurance",
         "land trust", "last will", "lcc", "life", "limited", "limited partnership",
         "llc", "llp", "local", "lp", "ltd", "management", "manhattan", " mers ",
-        " na ", " na", " companies ", "companies", "american",
+        " na ", " na$", " companies ", "companies", "american",
         "memorial", "metropolitan", "mgmt", "ministers", "ministries",
         "missionaries", "mngmnt", "museum", "national", "neighborhood",
         "new york city", "nyc", "nys", "organization", "orthodox", "overseas",
@@ -120,11 +155,11 @@ dictionary_entity_slugs <-
 
     is_reversed_name <-
       (
-        person_name %>% str_count("\\, ") >= 1 &&
+        person_name %>% str_count("\\,") >= 1 &&
           humaniformat::suffix(x = person_name) %>% is.na()
       ) |
       (
-        person_name %>% str_count("\\, ") >= 1 &&
+        person_name %>% str_count("\\,") >= 1 &&
           humaniformat::salutation(x = person_name) %>% is.na()
       )
 
@@ -172,7 +207,8 @@ dictionary_entity_slugs <-
 
     party <-
       party %>% str_replace_all(" , ", ", ") %>%
-      gsub("\\s+", " ", .)
+      gsub("\\s+", " ", .) %>%
+      str_squish()
 
     hit_words <-
       entity_words %>% str_to_lower() %>% str_c(collapse = "|")
@@ -183,7 +219,10 @@ dictionary_entity_slugs <-
 
     hit_slugs <- hit_words %>%
       str_to_upper() %>%
-      str_c(" LP|ASSOCIATES|LLC|L.L.C.|LP|CORP|INC|COMPANY|COMPANIES|REALTY|TRUST|DEPARTMENT|CORPORATION|BANCORP|GOVERNMENT|INSTITUE|EQUITIES|FUND| NA | NA| BANK| BANK |NYCDOT|SVNG|BNK|BANK OF|RLTY| CORP| BK | BK|ENTERPRISESINC| ENTERPRISES|CITY |COMANY|NEW YORK|CONSTRUCTORS|CONSTRUCTRS|STREETCORP| STREET | STREET| HOLDING| HOLDING |RLTY|NATIONAL|INVESTORS|SAVINGS|PARTNERS|PARTNERSHIP|HOLDINGS|HOLDCO|L.P.|\\ LP", collapse = "|")
+      str_c(
+        " LP|ASSOCIATES|LLC|L.L.C.|LP|CORP| INC|COMPANY|COMPANIES|REALTY|TRUST|DEPARTMENT|CORPORATION|BANCORP|GOVERNMENT|INSTITUE|EQUITIES|FUND| NA | NA| BANK| BANK |NYCDOT|SVNG|BNK|BANK OF|RLTY| CORP| BK | BK|ENTERPRISESINC| ENTERPRISES|CITY |COMANY|NEW YORK|CONSTRUCTORS|CONSTRUCTRS|STREETCORP| STREET | STREET| HOLDING| HOLDING |RLTY|NATIONAL|INVESTORS|SAVINGS|PARTNERS|PARTNERSHIP|HOLDINGS|HOLDCO|L.P.|\\ LP",
+        collapse = "|"
+      )
 
     not_entity <-
       !party %>%
@@ -212,12 +251,13 @@ dictionary_entity_slugs <-
     isEstate <- party %>% str_detect("ESTATE|AS TRUSTEE")
 
     if (has_comma && party_length %in% c(2,3) && not_entity) {
-      party <- party %>%
+      party <-
+        party %>%
         str_replace_all("ESQ.,", "ESQ. ")
       data <-
         .parse_person_name(person_name = party) %>%
         mutate(UQ(variable) := party_original,
-               UQ(new_var) := nameFull,
+               UQ(new_var) := nameFull %>%  str_remove_all("\\,$"),
                UQ(is_var) := FALSE,
                isEstate)
       return(data)
@@ -229,12 +269,12 @@ dictionary_entity_slugs <-
     }
     if (is_entity) {
       party_clean <-
-        party_original %>% str_remove_all("\\,|\\.") %>% gsub("\\s+", " ", .)
+        party_original %>% str_remove_all("\\,|\\.") %>% gsub("\\s+", " ", .) %>% str_remove_all("\\,$")
 
       data <-
         tibble(UQ(variable) := party_original,
-                   UQ(new_var) := party_clean,
-                   isEstate) %>%
+               UQ(new_var) := party_clean,
+               isEstate) %>%
         mutate(UQ(is_var) := TRUE)
       return(data)
     }
@@ -244,11 +284,11 @@ dictionary_entity_slugs <-
     data <-
       .parse_person_name(person_name = party) %>%
       mutate(UQ(variable) := party_original,
-             UQ(new_var) := nameFull,
+             UQ(new_var) := nameFull %>% str_remove_all("\\,$"),
              UQ(is_var) := FALSE,
              isEstate)
 
-   # suffixes <- c("ESQ", "MD")
+    # suffixes <- c("ESQ", "MD")
 
     #if (has_suffix) {}
 
@@ -278,7 +318,7 @@ resolve_data_parties <-
            variable = "nameOwnerPrimary") {
 
 
-    if (entity_words %>% purrr::is_null()) {
+    if (length(entity_words) == 0) {
       entity_words <- dictionary_entity_slugs()
     }
     .parse_party_data_safe <-
@@ -289,14 +329,17 @@ resolve_data_parties <-
       map_dfr(function(party) {
         glue::glue("Resolving: {party}") %>% message()
         .parse_party_data_safe(party = party,
-                         entity_words = entity_words,
-                         variable = variable)
+                               entity_words = entity_words,
+                               variable = variable)
       }) %>%
       dplyr::select(dplyr::matches("is[A-Z]"), dplyr::matches(variable), everything()) %>%
       dplyr::as_tibble() %>%
       suppressWarnings()
 
-
+    if (include_ethnicity) {
+      df_parties %>%
+        classify_acris_party_types()
+    }
 
 
     if (remove_first_last) {
@@ -305,14 +348,6 @@ resolve_data_parties <-
         dplyr::select(-one_of(c("nameFull", "nameFirst", "nameMiddle", "nameLast"))) %>%
         suppressWarnings()
     }
-    df_parties <- df_parties %>%
+    df_parties %>%
       dplyr::select(which(colMeans(is.na(.)) < 1))
-
-    if (include_ethnicity) {
-      vars <- names(df_parties)[names(df_parties) %>% str_detect(variable)]
-
-      df_parties <- df_parties %>%
-        .classify_names(name_column = vars[vars %>% str_detect("Resolved")], include_name_type = T)
-    }
-    df_parties
   }
